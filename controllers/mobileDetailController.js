9fcf408ec59c7e34dcc1b39c224552b6eda148eb
hafishali <hafishali4@gmail.com>
2024-10-23 17:32:12 +0530

    mobile service api completed

--- controllers/mobileDetailController.js
+++ controllers/mobileDetailController.js
@@ -1,17 +1,15 @@
 const mobile_dts = require('../models/mobileDetails');
 const user_dts = require('../models/User');
-
+const UserMobileDetails = require('../models/service'); 
 exports.addMobileDetails = async (req, res) => {
     try {
         const { model_name, company_name, name, mobileNumber, houseName, state, district, pincode } = req.body;
         const mobile_img = req.file.path;
 
-     
         if (!model_name || !company_name || !mobile_img) {
             return res.status(400).json({ error: 'All fields for mobile details are required.' });
         }
 
-        
         const newMobileDetails = new mobile_dts({
             model_name,
             company_name,
@@ -25,7 +23,6 @@ exports.addMobileDetails = async (req, res) => {
             return res.status(500).json({ error: 'Failed to save mobile details.' });
         }
 
-        
         if (!name || name.trim() === '') {
             return res.status(400).json({ error: 'Name is required' });
         }
@@ -50,10 +47,8 @@ exports.addMobileDetails = async (req, res) => {
             return res.status(400).json({ error: 'Pincode must be a valid 6-digit number' });
         }
 
-        
         const newUser = new user_dts({ name, mobileNumber, houseName, state, district, pincode });
         
-       
         try {
             await newUser.save();
         } catch (error) {
@@ -61,11 +56,24 @@ exports.addMobileDetails = async (req, res) => {
             return res.status(500).json({ error: 'Failed to create user.' });
         }
 
-        
+        const userMobileDetails = new UserMobileDetails({
+            userId: newUser._id,
+            mobileId: newMobileDetails._id 
+        });
+
+       
+        try {
+            await userMobileDetails.save();
+        } catch (error) {
+            console.error('Error saving user-mobile details:', error);
+            return res.status(500).json({ error: 'Failed to save user-mobile relationship.' });
+        }
+
         return res.status(201).json({
             message: 'Mobile details added successfully and user created.',
             mobileDetails: newMobileDetails,
-            user: newUser 
+            user: newUser,
+            userMobileDetails 
         });
     } catch (error) {
         console.error('Error occurred while adding mobile details:', error); 
